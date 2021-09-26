825e38aec66b859bf7fafd4ea87a3cf9a3c9c8d5
Zuza <zuza.ratajczyk@gmail.com>
2021-09-26 22:09:39 +0200

    Last stage working

--- university.py
+++ university.py
@@ -7,29 +7,48 @@ class Department:
         self.num_of_free_places = n
         self.exam = exam
 
+    def _get_candidate_result(self, candidate):
+        special_exam_result = candidate.results["special_exam"]
+        exam_result = candidate.results[self.exam]
+        return special_exam_result if special_exam_result > exam_result else exam_result
+
+    def sort_students(self, students):
+        def get_tuple(student):
+            return -(self._get_candidate_result(student)), student.first_name
+        students.sort(key=get_tuple)
+
+    def recruit(self, candidates, stage):
+        self.sort_students(candidates)
+        for applicant in candidates:
+            if self.num_of_free_places == 0:
+                break
+            if applicant.recruitment_departments[stage] == self.name:
+                self.accepted_students.append(applicant)
+                self.num_of_free_places -= 1
+        for student in self.accepted_students:
+            if student in candidates:
+                candidates.remove(student)
+
     def add_student(self, student):
         self.accepted_students.append(student)
 
-    def sort_students(self):
-        self.accepted_students.sort(key=lambda stud: (-float(stud.results[self.exam]), stud.first_name))
-
     def show_accepted(self):
         print(self.name)
-        self.sort_students()
+        self.sort_students(self.accepted_students)
         for student in self.accepted_students:
-            print(student.first_name, student.last_name, student.results[self.exam])
+            print(student.first_name, student.last_name, self._get_candidate_result(student))
 
-    def __add__(self, other):
-        self.num_of_free_places += other
+    def write_accepted_to_file(self):
+        self.sort_students(self.accepted_students)
+        with open(f"{self.name.lower()}.txt", "w") as f:
+            for student in self.accepted_students:
+                f.write(student.first_name + " " + student.last_name + " " + str(self._get_candidate_result(student)) + "\n")
 
-    def __sub__(self, other):
-        self.num_of_free_places -= other
 
-
-class Student:
+class Candidate:
 
     def __init__(self, first_name, last_name):
-        self.results = {"physics": 0.0, "chemistry": 0.0, "math": 0.0, "computer_science": 0.0}
+        self.results = {"physics": 0.0, "chemistry": 0.0, "math": 0.0, "computer_science": 0.0, "special_exam": 0.0}
         self.recruitment_departments = {"first_priority": "", "second_priority": "", "third_priority": ""}
         self.first_name = first_name
         self.last_name = last_name
@@ -44,9 +63,9 @@ class Student:
         self.results[subject1 + " + " + subject2] = (self.results[subject1] + self.results[subject2]) / 2
 
 
-def create_student(line_from_file):
+def create_applicant(line_from_file):
     applicants_lst = line_from_file.split()
-    applicant_obj = Student(applicants_lst[0], applicants_lst[1])
+    applicant_obj = Candidate(applicants_lst[0], applicants_lst[1])
     idx = 2
     for subject in applicant_obj.results.keys():
         applicant_obj.add_result(subject, float(applicants_lst[idx]))
@@ -62,40 +81,15 @@ def create_student(line_from_file):
 
 def read_from_file():
     list_of_applicants = []
-    file = open("applicant_list_6.txt", "r")
-    for line in file:
-        list_of_applicants.append(create_student(line))
-    file.close()
+    with open("applicant_list_7.txt", "r") as file:
+        for line in file:
+            list_of_applicants.append(create_applicant(line))
     return list_of_applicants
-
-
-def write_to_file(department):
-    f = open(f"{department.name.lower()}.txt", "w")
-    department.sort_students()
-    for student in department.accepted_students:
-        f.write(student.first_name + " " + student.last_name + " " + str(student.results[department.exam]) + "\n")
-    f.close()
-
-
-def choose_best_candidates(list_of_candidates, department, priority):
-    best_candidates = []
-    for applicant in list_of_candidates:
-        if department.num_of_free_places == 0:
-            break
-        if applicant.recruitment_departments[priority] == department.name:
-            best_candidates.append(applicant)
-            department.num_of_free_places -= 1
-    for applicant in best_candidates:
-        list_of_candidates.remove(applicant)
-    return best_candidates
 
 
 def recruitment(list_of_applicants, departments_list, stage):
     for department in departments_list:
-        list_of_applicants.sort(key=lambda app: (-(app.results[department.exam]), app.first_name))
-        accepted = choose_best_candidates(list_of_applicants, department, stage)
-        for student in accepted:
-            department.add_student(student)
+        department.recruit(list_of_applicants, stage)
 
 
 def main():
@@ -110,10 +104,11 @@ def main():
     recruitment(list_of_applicants, departments_list, "second_priority")
     recruitment(list_of_applicants, departments_list, "third_priority")
     for department in departments_list:
-        department.show_accepted()
+        department.write_accepted_to_file()
         print("\n")
     for department in departments_list:
-        write_to_file(department)
+        department.show_accepted()
+        print("\n")
 
 
 if __name__ == "__main__":
